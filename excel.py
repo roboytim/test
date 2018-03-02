ed70d608de5e52bf480f9f799d2cb16b9532afd3
roboytim <mike.19870320@gmail.com>
2018-03-03 00:37:49 +0800

    1

--- excel.py
+++ excel.py
@@ -13,40 +13,70 @@ for a in range(1,len(sys.argv)):
 path = add[0]
 '''
 #Excel文件路径
-path = r'/Users/roboytim/Desktop/work/exceltest/test.nmon.xlsx'
+path = r'C:\Users\Tim.Robert\Desktop\test\localhost_150706_1651.nmon.xlsx'
 #梯度数量
-num = 3
+num = 2
 #实例化工作表，打开制定excel文件
 book = xlrd.open_workbook(path)
 #打印改文件的所有工作表名称
-print (book.sheet_names())
+#print (book.sheet_names())
 #通过工作表索引获取sheet对象
 sheetbyindex = book.sheet_by_index(1)
-print (sheetbyindex.name)
+#print (sheetbyindex.name)
+#获取解析后的系统类型Linux||AIX
+for i in range(book.sheet_by_name('BBBP').nrows-1):
+    #print (type(book.sheet_by_name('BBBP').cell_value(i,1)))
+    #if "Linux" in book.sheet_by_name('BBBP').cell_value(i,1):
+    if book.sheet_by_name('BBBP').cell_value(i,1).find("Linux") == -1:
+        OS = 'Linux'
+    else:
+        OS = 'AIX'
+#print (OS)
 #通过工作表名称获取sheet对象
 CPUsheet = book.sheet_by_name('CPU_ALL')
-MEMsheet = book.sheet_by_name('MEM')
+#如果系统为linux，内存计算使用MEM工作表，如果为AIX，使用MEMNEW工作表
+if OS == 'Linux':
+    MEMsheet = book.sheet_by_name('MEM')
+else:
+    MEMsheet = book.sheet_by_name('MEMNEW')
+DISKsheet = book.sheet_by_name('DISKBUSY')
+print (DISKsheet.nrows,DISKsheet.ncols)
+#获取DISKBUSY中总和最大的列
+DISKmax=0
+for i in range(DISKsheet.ncols-1):
+    DISKtotal = 0
+    for j in range(DISKsheet.nrows-7):
+        print (type(DISKsheet.cell_value(j+1,i+1)))
+        DISKtotal = DISKtotal + DISKsheet.cell_value(j+1,i+1)
+        if DISKtotal > DISKmax:
+            DISKmax = DISKtotal
+            DISKclos = i+1
 #打印工作表名称，工作表行数，工作表列数
-print ('工作表名称：%s,工作表行数：%d，工作表列数：%d' % (CPUsheet.name,CPUsheet.nrows,CPUsheet.ncols))
+#print ('工作表名称：%s,工作表行数：%d，工作表列数：%d' % (CPUsheet.name,CPUsheet.nrows,CPUsheet.ncols))
 #计算每个梯度包含的行数
 rownum = int((CPUsheet.nrows-3)/num)
 #rownum = (CPUsheet.nrows-3)/num
-print (rownum)
+#print (rownum)
 #定义一个名称为CPU的数组
 CPU = []
 MEM = []
+DISK = []
 #打印输出工作表最后一列的数据
 for j in range(num):
     CPU_sum = 0
     MEM_sum = 0
+    DISK_sum = 0
     for i in range(rownum):
         #print (CPUsheet.cell_value(i+1,CPUsheet.ncols-1))
-        CPU_sum = CPU_sum + CPUsheet.cell_value(rownum * j + i + 1,CPUsheet.ncols-1)
+        CPU_sum = CPU_sum + CPUsheet.cell_value(rownum * j + i + 1,CPUsheet.ncols-2)
         # 打印内存使用率 公式为：(memtotal-memfree-cached-buffers)/memtotal*100
         MEM_sum = MEM_sum + (MEMsheet.cell_value(rownum * j + i + 1, 1) - MEMsheet.cell_value(rownum * j + i + 1, 5) - MEMsheet.cell_value(rownum * j + i + 1,10) - MEMsheet.cell_value(rownum * j + i + 1, 13)) / MEMsheet.cell_value(rownum * j + i + 1, 1) * 100
+        DISK_sum = DISK_sum + DISKsheet.cell_value(rownum * j + i + 1,DISKclos)
     CPU.append(str(CPU_sum))
     MEM.append(str(MEM_sum))
+    DISK.append(str(DISK_sum))
 
 for i in range(len(CPU)):
     print (i,CPU[i])
     print (i,MEM[i])
+    print (i,DISK[i])

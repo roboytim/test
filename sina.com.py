117627cb82e3e25e591dbdff16e32780bf0282a4
roboytim <mike.19870320@gmail.com>
2018-03-03 21:25:40 +0800

    1

---
+++ sina.com.py
@@ -0,0 +1,10 @@
+import requests
+res = requests.get('http://news.sina.com.cn/china/')
+#输出响应码200
+print (res)
+#输出res的默认编码格式
+print (res.encoding)
+#更改res的编码方式为UTF-8
+res.encoding = 'utf-8'
+#输出文本内容
+print (res.text)
